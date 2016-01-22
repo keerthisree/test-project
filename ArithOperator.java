40e121a0cd48339268bb5bd9012ab0a38ecb1bef
keerthisree <keerthi.kancherla@gmail.com>
2016-01-21 20:10:15 -0500

    adding java file

---
+++ ArithOperator.java
@@ -0,0 +1,16 @@
+public class ArithOperator{
+public static void main(String args[]){
+
+int a=30,b=20;
+int c=a+b;
+int d=a-b;
+int e=a*b;
+float f=a/b;
+
+System.out.println("value is:"+ c);
+System.out.println("value is :"+ d);
+System.out.println("value is :"+ e);
+System.out.println("value is:"+ f);
+
+}
+}
